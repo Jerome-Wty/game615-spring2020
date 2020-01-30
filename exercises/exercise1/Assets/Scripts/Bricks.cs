3ee05d3ffc68dd3604314bf7d48dfb6543cece8b
Jerome-Wty <tw4903a@student.american.edu>
2020-01-29 20:51:23 -0500

    exercise1

---
+++ exercises/exercise1/Assets/Scripts/Bricks.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bricks : MonoBehaviour
+{
+    public GameObject brickParticle;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ exercises/exercise1/Assets/Scripts/GM.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GM : MonoBehaviour{
+
+    public int lives = 3;
+    public int bricks = 20;
+    public float resetDelay = 1f;
+    public GameObject bricksPrefab;
+    public GameObject paddle;
+    public static GM instance = null;
+
+    private GameObject clonePaddle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+
+        Setup();
+
+    }
+    public void Setup()
+    {
+        clonePaddle = Instantiate(paddle, transform.position, Quaternion.identity) as GameObject;
+        Instantiate(bricksPrefab, bricksPrefab.transform.position, Quaternion.identity);
+
+    }
+
+
+    void SetupPaddle()
+    {
+        clonePaddle = Instantiate(paddle, transform.position, Quaternion.identity) as GameObject;
+    }
+    // Update is called once per frame
+
+}
