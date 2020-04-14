197502e91df6bbcae419bf27d69b4bfa6e5e3b86
Jerome-Wty <tw4903a@student.american.edu>
2020-04-14 12:09:26 -0400

    exercise3

---
+++ exercises/exercise3/Assets/_Scripts/UI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour
+{
+    public void White()
+    {
+        GameState.playerColor = "White";
+        AI.ai_player = "Black";
+        InputController.playerColor = "White";
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Black()
+    {
+        GameState.playerColor = "Black";
+        AI.ai_player = "White";
+        InputController.playerColor = "Black";
+        SceneManager.LoadScene("Main");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
